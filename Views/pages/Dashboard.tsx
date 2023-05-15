db5095f05eb57cd4fcf78828838360d4a405213d
sandeep514 <sandy.singh51480@gmail.com>
2023-05-16 01:17:45 +0530

    add color to pending orders

--- Views/pages/Dashboard.tsx
+++ Views/pages/Dashboard.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React, { useCallback, useEffect ,useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import type { PropsWithChildren } from 'react';
 import {
 	Pressable,
@@ -39,37 +39,37 @@ import { useIsFocused } from '@react-navigation/native'
 import NetInfo from "@react-native-community/netinfo";
 
 function Dashboard({ navigation }): JSX.Element {
-	const [ loader , setLoader] = useState(false);
-	const [ role , setRole] = useState();
-	const [ selectedOrderData , SetSelectedOrderData] = useState('');
-	const [pending , setPending] = useState({});
-	const [ready , setReady] = useState({});
-	const [delivered , setDelivered] = useState({});
-	const [ allOrdersList , setAllOrdersList] = useState([]);
+	const [loader, setLoader] = useState(false);
+	const [role, setRole] = useState();
+	const [selectedOrderData, SetSelectedOrderData] = useState('');
+	const [pending, setPending] = useState({});
+	const [ready, setReady] = useState({});
+	const [delivered, setDelivered] = useState({});
+	const [allOrdersList, setAllOrdersList] = useState([]);
 
 	useEffect(() => {
 		NetInfo.fetch().then(state => {
 			console.log(state);
-			if( state.type == "cellular" ){
+			if (state.type == "cellular") {
 				console.log("You are using mobile data to upload 30MB data. ");
-			}else{
-				if( 500 > 400 ){
+			} else {
+				if (500 > 400) {
 					console.log("sixe of attachment is 2MB, this will take  ");
 				}
 			}
-		  });
-	} , [])
+		});
+	}, [])
 	const wait = (timeout) => { // Defined the timeout function for testing purpose
 		return new Promise(resolve => setTimeout(resolve, timeout));
 	}
-	
+
 	const [isRefreshing, setIsRefreshing] = useState(false);
 	const isFocused = useIsFocused()
 
 	const onRefresh = useCallback(() => {
-			setIsRefreshing(true);
-			getData();
-			wait(2000).then(() => setIsRefreshing(false));
+		setIsRefreshing(true);
+		getData();
+		wait(2000).then(() => setIsRefreshing(false));
 	}, []);
 
 	const isDarkMode = useColorScheme() === 'dark';
@@ -77,29 +77,30 @@ function Dashboard({ navigation }): JSX.Element {
 		AsyncStorage.getItem('role').then((roleId) => {
 			console.log(roleId);
 			setRole(roleId);
-		}).catch((err) =>{
+		}).catch((err) => {
 			// console.log(err);
 		})
 		getData();
-		
-	} , [])
+
+	}, [])
 	useEffect(() => {
-		
+
 		getData();
-		
-	} , [isFocused])
+
+	}, [isFocused])
 
 	const getData = () => {
 		setLoader(true)
 		AsyncStorage.getItem('id').then((token) => {
-			let postedData = { api_token : token};
-			get('/orders/list/pending' , postedData).then((res) => {
+			let postedData = { api_token: token };
+			get('/orders/list/pending', postedData).then((res) => {
 				console.log(res);
 				let pending = res.data.data.data;
-			
+
 				setPending(pending);
 				setLoader(false)
 			}).catch((err) => {
+				console.log(err)
 				setLoader(false)
 			});
 
@@ -107,51 +108,60 @@ function Dashboard({ navigation }): JSX.Element {
 			setLoader(false)
 		});
 	}
-	
-	const Item = ({item}:any) => (
-		<Pressable onPress={() => { navigation.push('orderEdit' , { 'orderData' : item}) }} style={styles.item}>
-			<View style={[{} , flexDirectionRow]}>
-				<View style={[marginRight10,{width:'60%',overflow: 'hidden' }]}>
-					<View style={[{} , flexDirectionRow]}> 
-						<Text style={[{fontWeight: 'bold'},h5,gulluFont,marginRight10]}>Order Number</Text>
-						<Text style={[{marginTop: 0},h5,gulluFont]}>{item?.order_number}</Text>
+
+	const Item = ({ item }: any) => (
+		<Pressable onPress={() => { navigation.push('orderEdit', { 'orderData': item }) }} style={[(item?.pending_days > 15) ? styles.itemGreen : ((item?.pending_days <= 15 && item.pending_days >= 10) ? styles.itemYellow : styles.itemRed)]}>
+
+			<View style={[{}, flexDirectionRow]}>
+				<View style={[marginRight10, { width: '60%', overflow: 'hidden' }]}>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold' }, h5, gulluFont, marginRight10]}>Order Number</Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.order_number}</Text>
 					</View>
-					<View style={[{} , flexDirectionRow]}> 
-						<Text style={[{fontWeight: 'bold'},h5,gulluFont,marginRight10]}>Item </Text>
-						<Text style={[{marginTop: 0},h5,gulluFont]}>{item?.item} </Text>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold' }, h5, gulluFont, marginRight10]}>Item </Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.item} </Text>
 					</View>
-					<View style={[{} , flexDirectionRow]}> 
-						<Text style={[{fontWeight: 'bold'},h5,gulluFont,marginRight10]}>Color</Text>
-						<Text style={[{marginTop: 0},h5,gulluFont]}>{item?.color}</Text>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold' }, h5, gulluFont, marginRight10]}>Color</Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.color}</Text>
 					</View>
-					{(role == 1)?
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold' }, h5, gulluFont, marginRight10]}>Days</Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.pending_days}</Text>
+					</View>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold' }, h5, gulluFont, marginRight10]}>Days</Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.ready_date}</Text>
+					</View>
+					{(role == 1) ?
 						<View>
-							<View style={[{} , flexDirectionRow]}> 
-								<Text style={[{fontWeight: 'bold'},h5,gulluFont,marginRight10]}>Salesman</Text>
-								<Text style={[{marginTop: 0},h5,gulluFont]}>{item?.salesman?.name}</Text>
+							<View style={[{}, flexDirectionRow]}>
+								<Text style={[{ fontWeight: 'bold' }, h5, gulluFont, marginRight10]}>Salesman</Text>
+								<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.salesman?.name}</Text>
 							</View>
-							<View style={[{} , flexDirectionRow]}> 
-								<Text style={[{fontWeight: 'bold'},h5,gulluFont,marginRight10]}>Vendor</Text>
-								<Text style={[{marginTop: 0},h5,gulluFont]}>{item?.vendor?.name}</Text>
+							<View style={[{}, flexDirectionRow]}>
+								<Text style={[{ fontWeight: 'bold' }, h5, gulluFont, marginRight10]}>Vendor</Text>
+								<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.vendor?.name}</Text>
 							</View>
 
-						</View>					
-					:
+						</View>
+						:
 						null
 					}
-					
+
 
 				</View>
-				<View style={{width: '40%'}}>
-					{( item?.attachments?.length > 0)? <ImageBackground source={{uri: imagePath+''+item?.attachments[0].attachment }} resizeMode="contain" style={{height: 100 , width: '100%'}} /> : ''}
+				<View style={{ width: '40%' }}>
+					{(item?.attachments?.length > 0) ? <ImageBackground source={{ uri: imagePath + '' + item?.attachments[0].attachment }} resizeMode="contain" style={{ height: 100, width: '100%' }} /> : ''}
 				</View>
 			</View>
-			
-			
+
+
 		</Pressable>
-  );
+	);
 	return (
-		<SafeAreaView style={{backgroundColor: '#ededed'}}>
+		<SafeAreaView style={{ backgroundColor: '#ededed' }}>
 			<StatusBar
 				backgroundColor={gulluColor}
 			/>
@@ -163,31 +173,31 @@ function Dashboard({ navigation }): JSX.Element {
 
 					<View style={[{}, height83]} >
 						<View>
-							{(loader)? 
-								<ActivityIndicator  size={20} color={gulluColor} />
-							:
-							(pending?.length > 0)?
-								<FlatList
-									refreshing={isRefreshing} // Added pull to refesh state
-									onRefresh={onRefresh} // Added pull to refresh control
-									data={pending}
-									renderItem={({item}) => <Item item={item} />}
-									keyExtractor={item => item?.id}
-									showsVerticalScrollIndicator={false}
-								/>
+							{(loader) ?
+								<ActivityIndicator size={20} color={gulluColor} />
 								:
-								<View style={{justifyContent: 'center'}}>
-									<Text style={[h4,{color: gulluColor,textAlign: 'center'},marginTop30]}>No pending orders available</Text>
-								</View>
+								(Object.values(pending)?.length > 0) ?
+									<FlatList
+										refreshing={isRefreshing} // Added pull to refesh state
+										onRefresh={onRefresh} // Added pull to refresh control
+										data={Object.values(pending)}
+										renderItem={({ item }) => <Item item={item} />}
+										keyExtractor={item => item?.id}
+										showsVerticalScrollIndicator={false}
+									/>
+									:
+									<View style={{ justifyContent: 'center' }}>
+										<Text style={[h4, { color: gulluColor, textAlign: 'center' }, marginTop30]}>No pending orders available</Text>
+									</View>
 							}
-							
-							
+
+
 						</View>
-						{(role != undefined && role == 1)?
-							<Pressable onPress={() => { navigation.push('ordercreate') }} style={[{backgroundColor: gulluColor,height: 70 ,width: 70,padding: 0,margin:0 ,borderRadius: 100,right: 10,position: 'absolute',bottom: 0}]}>
-								<Text style={[{fontSize: 50,padding: 0,margin: 0,top: -5,color: goldenColor}, textAlignCenter]}>+</Text>
+						{(role != undefined && role == 1) ?
+							<Pressable onPress={() => { navigation.push('ordercreate') }} style={[{ backgroundColor: gulluColor, height: 70, width: 70, padding: 0, margin: 0, borderRadius: 100, right: 10, position: 'absolute', bottom: 0 }]}>
+								<Text style={[{ fontSize: 50, padding: 0, margin: 0, top: -5, color: goldenColor }, textAlignCenter]}>+</Text>
 							</Pressable>
-						:
+							:
 							null
 						}
 					</View>
@@ -217,14 +227,27 @@ const styles = StyleSheet.create({
 	highlight: {
 		fontWeight: '700',
 	},
-	item: {
-		backgroundColor: '#fff',
-		elevation:2,
+	itemRed: {
+		backgroundColor: 'red',
 		padding: 15,
 		marginVertical: 8,
 		marginHorizontal: 16,
 		borderRadius: 10
-	  },
+	},
+	itemYellow: {
+		backgroundColor: 'yellow',
+		padding: 15,
+		marginVertical: 8,
+		marginHorizontal: 16,
+		borderRadius: 10
+	},
+	itemGreen: {
+		backgroundColor: 'lightgreen',
+		padding: 15,
+		marginVertical: 8,
+		marginHorizontal: 16,
+		borderRadius: 10
+	},
 });
 
 export default Dashboard;
--- Views/pages/OrderList.tsx
+++ Views/pages/OrderList.tsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
 	FlatList,
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  ImageBackground,
-  View,
+	Pressable,
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	useColorScheme,
+	ImageBackground,
+	View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+	Colors,
+	DebugInstructions,
+	Header,
+	LearnMoreLinks,
+	ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import { cardBackgroundColor, h1, h2, h3, height10, height100, padding10, height6, height80, height85, height9, height90, justifyContentCenter, padding15, padding20, primaryBackgroundColor, primaryColor, screenheight, secondaryBackgroundColor, textAlignCenter, inputStyle, height15, flexDirectionRow, marginRight10, h5, h4 ,gulluColor,primaryGulluLightBackgroundColor} from '../assets/styles';
+import { cardBackgroundColor, h1, h2, h3, height10, height100, padding10, height6, height80, height85, height9, height90, justifyContentCenter, padding15, padding20, primaryBackgroundColor, primaryColor, screenheight, secondaryBackgroundColor, textAlignCenter, inputStyle, height15, flexDirectionRow, marginRight10, h5, h4, gulluColor, primaryGulluLightBackgroundColor } from '../assets/styles';
 import InputConponents from '../components/InputComponents';
 import HeaderComponent from '../components/HeaderComponent';
 import FooterComponent from '../components/FooterComponent';
@@ -33,29 +33,29 @@ import { goldenColor } from '../assets/styles';
 import { gulluFont } from '../assets/styles';
 
 type SectionProps = PropsWithChildren<{
-  title: string;
+	title: string;
 }>;
 
 
-function OrderList({navigation}): JSX.Element {
-  	const isDarkMode = useColorScheme() === 'dark';
+function OrderList({ navigation }): JSX.Element {
+	const isDarkMode = useColorScheme() === 'dark';
 
 
-	const [loader , setLoader] = useState(false);
-	const [pending , setPending] = useState({});
-	const [ready , setReady] = useState({});
-	const [delivered , setDelivered] = useState({});
-	const [ selectedOrderStatus , SetSelectedOrderStatus] = useState('');
-	const [ allOrdersList , setAllOrdersList] = useState([]);
-	const [ searchableData , setSearchableData] = useState([]);
-	
-	const [ selectedOrderData , SetSelectedOrderData] = useState('');
+	const [loader, setLoader] = useState(false);
+	const [pending, setPending] = useState({});
+	const [ready, setReady] = useState({});
+	const [delivered, setDelivered] = useState({});
+	const [selectedOrderStatus, SetSelectedOrderStatus] = useState('');
+	const [allOrdersList, setAllOrdersList] = useState([]);
+	const [searchableData, setSearchableData] = useState([]);
 
-	useEffect(()=> {
+	const [selectedOrderData, SetSelectedOrderData] = useState('');
+
+	useEffect(() => {
 		AsyncStorage.getItem('role').then((userRole) => {
 			console.log(userRole);
 		})
-	} , [])
+	}, [])
 	const backgroundStyle = {
 		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
 	};
@@ -63,12 +63,12 @@ function OrderList({navigation}): JSX.Element {
 		SetSelectedOrderStatus('pending');
 		AsyncStorage.getItem('id').then((token) => {
 			setLoader(true)
-			let postedData = { role: 'salesman',api_token : token};
-			get('/orders/list' , postedData).then((res) => {
+			let postedData = { role: 'salesman', api_token: token };
+			get('/orders/list', postedData).then((res) => {
 				let pending = res.data.data.data['pending'];
 				let ready = res.data.data.data['ready'];
 				let delivered = res.data.data.data['delivered'];
-				
+
 				setPending(pending);
 				setReady(ready);
 				setDelivered(delivered);
@@ -88,15 +88,15 @@ function OrderList({navigation}): JSX.Element {
 		}).catch((err) => {
 			setLoader(false)
 		});
-	} , [])
+	}, [])
 
 
 	const searchOrder = (searchableText) => {
 		let newSearchableArray = [];
-		if( allOrdersList.length > 0 ){
+		if (allOrdersList.length > 0) {
 			allOrdersList.filter((list) => {
 				let searchableLowercase = (list.order_number).toLowerCase();
-				if(searchableLowercase.includes((searchableText).toLowerCase())){ 
+				if (searchableLowercase.includes((searchableText).toLowerCase())) {
 					newSearchableArray.push(list)
 
 				}
@@ -104,122 +104,122 @@ function OrderList({navigation}): JSX.Element {
 			setSearchableData(newSearchableArray);
 			SetSelectedOrderData(newSearchableArray);
 		}
-		
+
 
 	}
-  
-  const Item = ({item}:any) => (
-		<Pressable onPress={() => { navigation.push('orderEdit' , { 'orderData' : item}) }} style={styles.item}>
-			<View style={[{} , flexDirectionRow]}>
-				<View style={[marginRight10,{width:'60%',overflow: 'hidden' }]}>
-					<View style={[{} , flexDirectionRow]}> 
-						<Text style={[{fontWeight: 'bold',width: '50%'},h5,marginRight10 ,gulluFont]}>Order Number</Text>
-						<Text style={[{marginTop: 0},h5,gulluFont]}>{item.order_number}</Text>
+
+	const Item = ({ item }: any) => (
+		<Pressable onPress={() => { navigation.push('orderEdit', { 'orderData': item }) }} style={styles.item}>
+			<View style={[{}, flexDirectionRow]}>
+				<View style={[marginRight10, { width: '60%', overflow: 'hidden' }]}>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold', width: '50%' }, h5, marginRight10, gulluFont]}>Order Number</Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.order_number}</Text>
 					</View>
-					<View style={[{} , flexDirectionRow]}> 
-						<Text style={[{fontWeight: 'bold',width: '50%'},h5,marginRight10 ,gulluFont]}>Item </Text>
-						<Text style={[{marginTop: 0},h5,gulluFont]}>{item.item} </Text>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold', width: '50%' }, h5, marginRight10, gulluFont]}>Item </Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.item} </Text>
 					</View>
-					<View style={[{} , flexDirectionRow]}> 
-						<Text style={[{fontWeight: 'bold',width: '50%'},h5,marginRight10 ,gulluFont]}>Color</Text>
-						<Text style={[{marginTop: 0},h5,gulluFont]}>{item.color}</Text>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold', width: '50%' }, h5, marginRight10, gulluFont]}>Color</Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.color}</Text>
 					</View>
-					<View style={[{} , flexDirectionRow]}> 
-						<Text style={[{fontWeight: 'bold',width: '50%'},h5,marginRight10 ,gulluFont]}>Salesman</Text>
-						<Text style={[{marginTop: 0},h5,gulluFont]}>{item.salesman.name}</Text>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold', width: '50%' }, h5, marginRight10, gulluFont]}>Salesman</Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.salesman?.name}</Text>
 					</View>
-					<View style={[{} , flexDirectionRow]}> 
-						<Text style={[{fontWeight: 'bold',width: '50%'},h5,marginRight10 ,gulluFont]}>Vendor</Text>
-						<Text style={[{marginTop: 0},h5,gulluFont]}>{item.vendor.name}</Text>
+					<View style={[{}, flexDirectionRow]}>
+						<Text style={[{ fontWeight: 'bold', width: '50%' }, h5, marginRight10, gulluFont]}>Vendor</Text>
+						<Text style={[{ marginTop: 0 }, h5, gulluFont]}>{item?.vendor?.name}</Text>
 					</View>
-					
+
 
 				</View>
-				<View style={{width: '40%'}}>
-					{( item.attachments.length > 0)? <ImageBackground source={{uri: imagePath+''+item.attachments[0].attachment }} resizeMode="contain" style={{height: 100 , width: '100%'}} /> : ''}
+				<View style={{ width: '40%' }}>
+					{(item?.attachments.length > 0) ? <ImageBackground source={{ uri: imagePath + '' + item?.attachments[0].attachment }} resizeMode="contain" style={{ height: 100, width: '100%' }} /> : ''}
 				</View>
 			</View>
-			
-			
+
+
 		</Pressable>
-  );
+	);
 	const checkOrderStatus = (changedOrderStatus) => {
 		SetSelectedOrderStatus(changedOrderStatus)
-		if( changedOrderStatus == 'pending' ) {
+		if (changedOrderStatus == 'pending') {
 			SetSelectedOrderData(pending);
-		}else if(changedOrderStatus == 'ready' ){
+		} else if (changedOrderStatus == 'ready') {
 			SetSelectedOrderData(ready);
-		}else if(changedOrderStatus == 'delivered' ){
+		} else if (changedOrderStatus == 'delivered') {
 			SetSelectedOrderData(delivered);
 
 		}
 	}
-  return (
-    <SafeAreaView style={{backgroundColor: '#ededed'}}>
+	return (
+		<SafeAreaView style={{ backgroundColor: '#ededed' }}>
 			<StatusBar
 				backgroundColor={gulluColor}
 			/>
 			<View style={[height100, primaryGulluLightBackgroundColor]}>
-			<View style={[{} , height100]}>
-				<View style={[{},height6]}>
-                    <HeaderComponent navigation={navigation} title="List Orders"  />
-                </View>
-				<View style={[{},height85]} >
-
-					{(loader)?
-						<ActivityIndicator size={30} color={gulluColor}/>
-
-					:
-						<View>
-							<View style={[{},height15]}>
-								<InputConponents placeholder="search order" style={[{} , inputStyleBlack]} inputValue={(value:any) => { searchOrder(value) }}/>
-							</View>
-							<View style={[{} , height85]}>
-								<View style={{justifyContent: 'space-around',flexDirection: 'row'}}>
-									<Pressable onPress={() => { checkOrderStatus('pending') }} style={[{width: '30%',borderRadius: 10},padding10,justifyContentCenter, (selectedOrderStatus == "pending")? {backgroundColor: goldenColor} : {borderColor: goldenColor , borderWidth: 2}]}>
-										<Text style={[{textAlign: 'center' , fontSize: 18} , (selectedOrderStatus == "pending")? { } : {color : secondaryBackgroundColor}]}>Pending</Text>
-									</Pressable>
-									<Pressable onPress={() => { checkOrderStatus('ready') }} style={[{width: '30%',borderRadius: 10},padding10,justifyContentCenter, (selectedOrderStatus == "ready")? {backgroundColor: goldenColor} : {borderColor: goldenColor , borderWidth: 2}]}>
-										<Text style={[{textAlign: 'center' , fontSize: 18} , (selectedOrderStatus == "ready")? {} : {color: secondaryBackgroundColor}]}>Ready</Text>
-									</Pressable>
-									<Pressable onPress={() => { checkOrderStatus('delivered') }} style={[{width: '30%',borderRadius: 10},padding10,justifyContentCenter, (selectedOrderStatus == "delivered")? {backgroundColor: goldenColor} : {borderColor: goldenColor , borderWidth: 2}]}>
-										<Text style={[{textAlign: 'center' , fontSize: 18} , (selectedOrderStatus == "delivered")? {} : {color: secondaryBackgroundColor}]}>Delivered</Text>
-									</Pressable>
-								</View>
-								<FlatList
-									data={selectedOrderData}
-									renderItem={({item}) => <Item item={item} />}
-									keyExtractor={item => item.id}
-									showsVerticalScrollIndicator={false}
-								/>
-							</View>
-							<Pressable onPress={() => { navigation.push('ordercreate') }} style={[{backgroundColor: gulluColor,height: 70 ,width: 70,padding: 0,margin:0 ,borderRadius: 100,right: 10,position: 'absolute',bottom: 0}]}>
-								<Text style={[{fontSize: 50,padding: 0,margin: 0,top: -3,color: goldenColor}, textAlignCenter]}>+</Text>
-							</Pressable>
-						</View>
-					}
+				<View style={[{}, height100]}>
+					<View style={[{}, height6]}>
+						<HeaderComponent navigation={navigation} title="List Orders" />
 					</View>
-				<View style={[{},height9]}>
-					<FooterComponent navigation={navigation} />
+					<View style={[{}, height85]} >
+
+						{(loader) ?
+							<ActivityIndicator size={30} color={gulluColor} />
+
+							:
+							<View>
+								<View style={[{}, height15]}>
+									<InputConponents placeholder="search order" style={[{}, inputStyleBlack]} inputValue={(value: any) => { searchOrder(value) }} />
+								</View>
+								<View style={[{}, height85]}>
+									<View style={{ justifyContent: 'space-around', flexDirection: 'row' }}>
+										<Pressable onPress={() => { checkOrderStatus('pending') }} style={[{ width: '30%', borderRadius: 10 }, padding10, justifyContentCenter, (selectedOrderStatus == "pending") ? { backgroundColor: goldenColor } : { borderColor: goldenColor, borderWidth: 2 }]}>
+											<Text style={[{ textAlign: 'center', fontSize: 18 }, (selectedOrderStatus == "pending") ? {} : { color: secondaryBackgroundColor }]}>Pending</Text>
+										</Pressable>
+										<Pressable onPress={() => { checkOrderStatus('ready') }} style={[{ width: '30%', borderRadius: 10 }, padding10, justifyContentCenter, (selectedOrderStatus == "ready") ? { backgroundColor: goldenColor } : { borderColor: goldenColor, borderWidth: 2 }]}>
+											<Text style={[{ textAlign: 'center', fontSize: 18 }, (selectedOrderStatus == "ready") ? {} : { color: secondaryBackgroundColor }]}>Ready</Text>
+										</Pressable>
+										<Pressable onPress={() => { checkOrderStatus('delivered') }} style={[{ width: '30%', borderRadius: 10 }, padding10, justifyContentCenter, (selectedOrderStatus == "delivered") ? { backgroundColor: goldenColor } : { borderColor: goldenColor, borderWidth: 2 }]}>
+											<Text style={[{ textAlign: 'center', fontSize: 18 }, (selectedOrderStatus == "delivered") ? {} : { color: secondaryBackgroundColor }]}>Delivered</Text>
+										</Pressable>
+									</View>
+									<FlatList
+										data={selectedOrderData}
+										renderItem={({ item }) => <Item item={item} />}
+										keyExtractor={item => item?.id}
+										showsVerticalScrollIndicator={false}
+									/>
+								</View>
+								<Pressable onPress={() => { navigation.push('ordercreate') }} style={[{ backgroundColor: gulluColor, height: 70, width: 70, padding: 0, margin: 0, borderRadius: 100, right: 10, position: 'absolute', bottom: 0 }]}>
+									<Text style={[{ fontSize: 50, padding: 0, margin: 0, top: -3, color: goldenColor }, textAlignCenter]}>+</Text>
+								</Pressable>
+							</View>
+						}
+					</View>
+					<View style={[{}, height9]}>
+						<FooterComponent navigation={navigation} />
+					</View>
 				</View>
 			</View>
-		</View>
-    </SafeAreaView>
-  );
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-	  flex: 1,
-	  marginTop: StatusBar.currentHeight || 0,
+		flex: 1,
+		marginTop: StatusBar.currentHeight || 0,
 	},
 	item: {
-	  backgroundColor: '#fff',
-	  padding: 15,
-	  marginVertical: 8,
-	  marginHorizontal: 16,
-	  borderRadius: 10
+		backgroundColor: '#fff',
+		padding: 15,
+		marginVertical: 8,
+		marginHorizontal: 16,
+		borderRadius: 10
 	},
-  });
+});
 
 export default OrderList;
